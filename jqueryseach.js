2af51fdf35c7d159cddea02138a6e93e8e264ce4
AlamRobi <rabiul@britishbanglatravel.com>
2019-10-17 13:02:28 +0600

    New jquery file are updated

---
+++ jqueryseach.js
@@ -0,0 +1,26 @@
+<
+script >
+    $('#search').keyup(function() {
+        search_table($(this).val());
+    });
+
+function search_table(value) {
+    $('#profilefolder tr').each(function() {
+
+        var found = 'false';
+        $(this).each(function() {
+
+            if ($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                found = 'true';
+            }
+        });
+        if (found == 'true') {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+}
+
+<
+/script>
